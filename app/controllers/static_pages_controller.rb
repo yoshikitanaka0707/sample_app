71b7d040e2af1b4cd70a4f1968ba8750ce0496b0
YoshikiTanaka <kishiedakinta@gmail.com>
2018-07-10 16:26:46 +0900

    ゴリラ

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,13 @@
 #/static_pages/homeというURLにアクセスすると、
 #RailsはStaticPagesコントローラを参照し、homeアクションに記述されているコードを実行する。
 class StaticPagesController < ApplicationController
+#StaticPagesControllerはRubyのクラスだが
+#ApplicationControllerクラスを継承しているため、
+#StaticPagesControllerのメソッドは (たとえ何も書かれていなくても)
+#Rails特有の振る舞いをします。
+#具体的には、/static_pages/homeというURLにアクセスすると、
+#RailsはStaticPagesコントローラを参照し、homeアクションに記述されているコードを実行します。
+#その後、そのアクションに対応するビューを出力します。  
   def home
   end
 
